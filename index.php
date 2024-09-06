a5dae9775c5c66f2b8ae8f9ff8e3715337340df2
DannyS12 <danny_hugh@rocketmail.com>
2024-09-06 01:27:06 -0600

    Arreglo css login

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+// Verificar si el usuario está autenticado
+if (isset($_SESSION['usuario_id'])) {
+    // Redirigir al dashboard si el usuario está autenticado
+    header("Location: dashboard");
+    exit();
+} else {
+    // Redirigir al login si el usuario no está autenticado
+    header("Location: login");
+    exit();
+}
+?>
